213b2ddba02366449723bcb1b9d01d42e75e1d97
Your Name <you@example.com>
2020-04-28 10:47:05 -0400

    .

--- front-end/get-users.js
+++ front-end/get-users.js
@@ -1,9 +1,12 @@
-const getUsers = () => {
+const getUsers = (id) => {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('loadend', (event) => {
     console.log(event.target.response);
   })
 
-  xhr.open('GET', 'http://localhost:3000/users');
+  xhr.open('GET', `http://localhost:3000/users/${id}`);
+  
   xhr.send();
 }
+/*taking in requests, making a request for data and changing file accordingly
+        
